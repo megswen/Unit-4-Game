3ac3007bb857982e5145c30cceafe9fe5ae7449f
Megan Swenson <mrsmeganswenson@gmail.com>
2020-03-20 22:40:14 -0600

    Uploading my finished crystal collector game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,59 +5,39 @@ var randomNumber = [30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 
 var crystalNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Create a variable that will make the computer choose a random number from the randomNumbers array
-var computerNumber = randomNumber[Math.floor(Math.random() * randomNumber.length)];
-    // Display it in the randomnumber-text div
-    $("#randomnumber-text").text(computerNumber);
+var computerNumber;
 
 // Create variables that will make each crystal choose a random number from their respective arrays
-var firstCrystalNumber = crystalNumbers[Math.floor(Math.random() * crystalNumbers.length)];
-var secondCrystalNumber = crystalNumbers[Math.floor(Math.random() * crystalNumbers.length)];
-var thirdCrystalNumber = crystalNumbers[Math.floor(Math.random() * crystalNumbers.length)];
-var fourthCrystalNumber = crystalNumbers[Math.floor(Math.random() * crystalNumbers.length)];
+var firstCrystalNumber;
+var secondCrystalNumber;
+var thirdCrystalNumber;
+var fourthCrystalNumber;
 
 // Create a variable that will hold the user's sum
-var userSum = 0;
-$("#usersum-text").text(userSum);
+var userSum;
 
-// Create cariables that hold wins and losses
+// Create variables that hold wins and losses
 var wins = 0;
 var losses = 0;
+
+// Run the reset function to start on a clear game
+reset();
 
 //Reset game function
 function reset() {
     userSum = 0;
     $('#usersum-text').text(userSum);
     computerNumber = randomNumber[Math.floor(Math.random() * randomNumber.length)];
+    $("#randomnumber-text").text(computerNumber);
     firstCrystalNumber = crystalNumbers[Math.floor(Math.random() * crystalNumbers.length)];
     secondCrystalNumber = crystalNumbers[Math.floor(Math.random() * crystalNumbers.length)];
     thirdCrystalNumber = crystalNumbers[Math.floor(Math.random() * crystalNumbers.length)];
     fourthCrystalNumber = crystalNumbers[Math.floor(Math.random() * crystalNumbers.length)];
 }
 
-$(document).ready(function(){
-
-    // Create four functions (one for each crystal) that will run whenever the user clicks on a crystal and adds the numbers in the usersum-text div
-    $('#crystal1').click(() => {
-        userSum = userSum + firstCrystalNumber;
-        $('#usersum-text').text(userSum);
-    });
-
-    $('#crystal2').click(() => {
-        userSum = userSum + secondCrystalNumber;
-        $('#usersum-text').text(userSum);
-    });
-
-    $('#crystal3').click(() => {
-        userSum = userSum + thirdCrystalNumber;
-        $('#usersum-text').text(userSum);
-    });
-
-    $('#crystal4').click(() => {
-        userSum = userSum + fourthCrystalNumber;
-        $('#usersum-text').text(userSum);
-    });
-
-    //If elwe statement that will increase wins if the user matches the random number and increases losses when the user goes above the random number
+function add(numberToAdd) {
+    userSum = userSum + numberToAdd;
+    $('#usersum-text').text(userSum);
     if (userSum === computerNumber) {
         wins++;
         alert("You won!");
@@ -66,9 +46,34 @@ $(document).ready(function(){
         losses++;
         alert("You lost!");
         reset();
-    } 
+    } else {
+    }
+    displayWinLoss();
+}
 
-    // Move wins and losses to html
+function displayWinLoss(){
     $("#wins-text").text("Your wins: " + wins);
     $("#losses-text").text("Your losses: " + losses);
+}
+
+$(document).ready(function(){
+
+    // Create four functions (one for each crystal) that will run whenever the user clicks on a crystal and adds the numbers in the usersum-text div
+    $('#crystal1').click(() => {
+        add(firstCrystalNumber);
+    });
+
+    $('#crystal2').click(() => {
+        add(secondCrystalNumber);
+    });
+
+    $('#crystal3').click(() => {
+        add(thirdCrystalNumber);
+    });
+
+    $('#crystal4').click(() => {
+        add(fourthCrystalNumber);
+    });
+
+    displayWinLoss();
 })
